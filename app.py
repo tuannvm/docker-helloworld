73c3e9b143709c93635d20141f0599dc931ace46
Tuan Nguyen <tuannvm@tuannvm.com>
2017-04-28 00:01:02 +0700

    change content

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def entry_point():
-    return 'Hello grumpy World!'
+    return 'Hello Grumpy World!'
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
